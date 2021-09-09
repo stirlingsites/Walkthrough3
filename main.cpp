aa06d5b714e77bccb273a6207cffb4c9640d37da
stirlingsites <89936754+stirlingsites@users.noreply.github.com>
2021-09-09 19:47:22 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+﻿//Stirling Sites
+//https://docs.microsoft.com/en-us/windows/uwp/cpp-and-winrt-apis/get-started?view=msvc-160
+#include "pch.h"
+
+using namespace winrt;
+using namespace Windows::Foundation;
+
+int main()
+{
+    init_apartment();
+    Uri uri(L"http://aka.ms/cppwinrt");
+    printf("Hello, %ls!\n", uri.AbsoluteUri().c_str());
+}
